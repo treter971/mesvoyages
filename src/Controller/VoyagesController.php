fc9d0c971ea653e084dfa92af765ba4819b3c3e4
PasBesoinDeCetteInformationBandeDeVoleur <treter971@gmail.com>
2026-09-22 15:41:38 +0200

    0.3.4 Utilisation de twig et création de multiple pages (j'ai vraiment juste oublier de commite la 0.3 my bad)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -5,9 +5,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AcceuilController extends AbstractController {
-    #[Route ('/', name: 'accueil')]
+class VoyagesController extends AbstractController {
+    #[Route ('/voyages', name: 'voyages')]
     public function index(): Response{
-        return $this->render("pages/accueil.html.twig");
+        return $this->render("pages/voyages.html.twig");
     }
 }
